a08c9fcc2278ccc7e89d86bee512ee843e1bb4c3
fsanchez2334 <fsanchez@cristoreyny.org>
2018-05-02 21:37:45 +0000

    Updated repo

--- src/WhoseOnline.js
+++ src/WhoseOnline.js
@@ -3,33 +3,40 @@ import React, { Component } from 'react';
 import Avatar from 'material-ui/Avatar';
 import {List, ListItem} from 'material-ui/List';
 import Subheader from 'material-ui/Subheader';
-// import firebase from 'firebase';
+import UserApi from './UserApi';
+import firebase from 'firebase';
 
 export default class WhoseOnline extends Component {
   
   // componentWillMount() {
-  //   var id = this.props.match.params.id;
-  //   var sessionDatabaseRef = firebase.database().ref("/session/" + id);
+  //   var id = this.props.match.params.id; //game id
+  //   var sessionDatabaseRef = firebase.database().ref("/session-metadata/" + id);
   //   var currentUser = firebase.auth().currentUser.uid;
   // }
+  constructor(props) {
+    super(props);
+    this.state = {peopleName: []};
+  }
   
   componentWillMount() {
-   
-
+    var id = this.props.match.params.id;
+    var sessionDatabaseRef = firebase.database().ref("/session-metadata/" + id);
+    var usersId = sessionDatabaseRef.child("users");
+    const arrayOfAllUsersName = usersId.map(x => {
+      return {
+        name: UserApi.getName(x),
+        img: UserApi.getPhotoUrl(x)
+      }
+    })
+    this.setState({
+      peopleName: arrayOfAllUsersName
+    });
+    
     
   }
   
   render() {
-    const people = [
-      {
-        name: "Brendan",
-        img: "sssfdf"
-      },
-      {
-        name: "Brendan",
-        img: "sssfdf"
-      }
-    ]
+    var people= this.state.peopleName
     
     const peopleList = people.map(function(person){
       return (
