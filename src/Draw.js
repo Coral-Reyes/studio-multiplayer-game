94be7cff2cf0bc81127a19cf963b4c7d09d27d94
fsanchez2334 <fsanchez@cristoreyny.org>
2018-05-09 22:14:55 +0000

    Added color features (614)

--- src/Draw.js
+++ src/Draw.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
 // import RaisedButton from 'material-ui/RaisedButton';
-// import UserApi from './UserApi.js';
+import UserApi from './UserApi.js';
 // import Avatar from 'material-ui/Avatar';
 // import { List, ListItem } from 'material-ui/List';
-// import firebase from 'firebase';
+import firebase from 'firebase';
 import WhoseOnline from './WhoseOnline.js';
 // import './Data.css';
 import randomWords from 'random-words';
@@ -19,19 +19,73 @@ export default class Draw extends Component {
       currX: 0,
       prevY: 0,
       currY: 0,
-      randomWord: ""
+      randomWord: "",
+      inputvalue: "",
+      repeat: "",
+      drawer: "",
+      peopleNames: [],
+      color: "#000"
     };
-    console.log(randomWords());
+    // console.log(randomWords());
     
  }
+ 
+  componentWillMount() {
+    var id = this.props.match.params.id;
+    var sessionDatabaseRef = firebase.database().ref("/session-metadata/" + id);
+    var usersId = sessionDatabaseRef.child("users");
+    usersId.on("value", (snapshot) => {
+      var container = snapshot.val();
+      const arrayOfAllUsersName = container.map(x => {
+        return {
+          name: UserApi.getName(x),
+          img: UserApi.getPhotoUrl(x)
+        }
+      })
+      this.setState({
+        peopleNames: arrayOfAllUsersName
+      });
+    })
+  }  
+ 
  componentDidUpdate() {
        this.draw();
  }
+ 
+ firstDrawer() {
+   this.setState({
+    drawer: this.state.peopleNames[0]
+   });
+ }
 
  genWord(){
+  var words = randomWords();
+  var id = this.props.match.params.id;
+  var sessionDatabaseRef = firebase.database().ref("/session-metadata/" + id);
+  sessionDatabaseRef.set({words: words});
   this.setState({
-   randomWord: randomWords()
+   randomWord: words
   }); 
+ }
+ 
+
+ getInputValue(){
+  var x = document.getElementById("guess").value;
+  this.setState({
+   inputvalue: x
+  });
+ }
+ 
+ wins(){
+  if (this.state.inputvalue === this.state.randomWord) {
+     this.setState({
+      randomWord: randomWords()
+     }); 
+  } else {
+     this.setState({
+      repeat: "False"
+     }); 
+  }
  }
 
  draw() {
@@ -43,25 +97,38 @@ export default class Draw extends Component {
        ctx.beginPath();
        ctx.arc(
         this.state.currX - canvas.getBoundingClientRect().left,
-        this.state.currY - canvas.getBoundingClientRect().top*2.5,
+        this.state.currY - canvas.getBoundingClientRect().top*3,
         5, 0,
         Math.PI * 2, 
         true
        );
+       ctx.strokeStyle= this.state.color;
        ctx.lineWidth = 2;
        ctx.stroke();
        ctx.closePath();
+       
  }
  
+ 
 
- onMouseMoved(e){
-   e.stopPropagation();
+onMouseMoved(e){
+    e.stopPropagation();
    this.setState({
       prevX: this.state.currX,
       currX: e.screenX,
       prevY: this.state.currX,
-      currY: e.screenY,
+     currY: e.screenY + 35,
     });
+}
+getRandomColor(){
+   var letters= "0123456789ABCDEF"
+   var color = "#"
+   for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    this.setState({
+     color: color
+    })
  }
  
  render() {
@@ -70,13 +137,17 @@ export default class Draw extends Component {
         <h1>Draw</h1>
         <button onClick={this.genWord.bind(this)}>New Word</button>
         <p>{this.state.randomWord}</p>
+        <button onClick={this.getRandomColor.bind(this)}> Change Color </button>
         <div id="canvas">
          <canvas ref="canvas" onMouseMove={this.onMouseMoved.bind(this)} id="can" width="400" height="400" style={{position: "absolute", top: "10%", left: "50%", border:"2px solid"}}></canvas>
         </div>
         <div id="online">
-         <WhoseOnline session={this.props.match.params.id}/>
-         <input type="text"/>
-         </div>
+         <WhoseOnline peopleNames={this.state.peopleNames} session={this.props.match.params.id}/>
+         <input type="text" id="guess" onChange={this.getInputValue.bind(this)}/>
+         <p>{this.state.inputvalue}</p>
+         <button onClick={this.wins.bind(this)}>Compare Answers</button>
+         <p>{this.state.repeat}</p>
+        </div>
      </div>
     
     );
--- src/WhoseOnline.js
+++ src/WhoseOnline.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 import Avatar from 'material-ui/Avatar';
 import {List, ListItem} from 'material-ui/List';
 import Subheader from 'material-ui/Subheader';
-import UserApi from './UserApi';
-import firebase from 'firebase';
+// import UserApi from './UserApi';
+// import firebase from 'firebase';
 
 export default class WhoseOnline extends Component {
   
@@ -13,31 +13,12 @@ export default class WhoseOnline extends Component {
   //   var sessionDatabaseRef = firebase.database().ref("/session-metadata/" + id);
   //   var currentUser = firebase.auth().currentUser.uid;
   // }
-  constructor(props) {
-    super(props);
-    this.state = {peopleName: []};
-  }
-  
-  componentWillMount() {
-    var id = this.props.session;
-    var sessionDatabaseRef = firebase.database().ref("/session-metadata/" + id);
-    var usersId = sessionDatabaseRef.child("users");
-    usersId.on("value", (snapshot) => {
-      var container = snapshot.val();
-      const arrayOfAllUsersName = container.map(x => {
-        return {
-          name: UserApi.getName(x),
-          img: UserApi.getPhotoUrl(x)
-        }
-      })
-      this.setState({
-        peopleName: arrayOfAllUsersName
-      });
-    })
-  }  
+  // constructor(props) {
+  //   super(props);
+  // }
   
   render() {
-    var people= this.state.peopleName
+    var people= this.props.peopleNames;
     
     const peopleList = people.map(function(person){
       return (
@@ -45,7 +26,7 @@ export default class WhoseOnline extends Component {
           primaryText={person.name}
           leftAvatar={<Avatar src={person.img} />}
         />
-      )
+      );
     });
     
     return (
